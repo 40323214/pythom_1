f618c9e2a95181c39d5353f407e8bf7f2b22ecea
Shin Wei <40323214@gm.nfu.edu.tw>
2014-12-29 10:12:31 +0800

    40323214

--- std/example2.py
+++ std/example2.py
@@ -21,7 +21,7 @@ class Application(object):
         # 你的 github repository url
         self.github_repo_url = 'https://github.com/Openandgit/2014cpb_final_project-'
         # 你的 openshift app
-        self.openshift_url = 'http://cpb-nfutaiwan.rhcloud.com/'
+        self.openshift_url = 'http://cp-nfumde.rhcloud.com/'
         # 你的自評
         self.evaluation = [('Project 7', 90), ('Project 8', 90)]
         # 你的照片 url
